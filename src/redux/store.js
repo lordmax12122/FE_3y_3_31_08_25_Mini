7b51ff1a5720caf1607a30316c05b18ab982ad0f
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-04-05 09:53:52 +0300

    main

---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+// Точка збору де ми збираємо повністю все що стосується редаксу
+
+// npm install @redux-devtools/extension - для підʼєднання інструментів розробника для старих версій редаксу
+
+// import { devToolsEnhancer } from "@redux-devtools/extension"; const enhancer = devToolsEnhancer();
